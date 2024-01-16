6c2c48c95e9b13a25fc6a4249b986e78ef74390b
vo6i <deooor1r@gmail.com>
2024-01-16 03:27:38 +0300

    new scripts ready for born

--- app.js
+++ app.js
@@ -19,6 +19,25 @@
         });
     }
 
+    let notification
+    let interval
+    document.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "hidden") {
+        const leaveDate = new Date()
+        interval = setInterval(() => {
+          notification = new Notification("Come back to dclxviclan", {
+            body: `You have been gone for ${Math.round(
+              (new Date() - leaveDate) / 1000
+            )} seconds`,
+            tag: "Come Back",
+          })
+         }, 100)
+       } else {
+         if (interval) clearInterval(interval)
+         if (notification) notification.close()
+       }
+    })
+
     // show an error message
     const showError = () => {
         const error = document.querySelector('.error');
